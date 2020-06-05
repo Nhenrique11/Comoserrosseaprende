39ebf399f733f099512ae388743b30e47bf8eff4
Nhenrique11 <66130436+Nhenrique11@users.noreply.github.com>
2020-06-05 10:46:40 +0000

    Algumas alterações como contador de cdo biscoito do UIlicles e animação

---
+++ Coesa/Assets/Scripts/cookieconter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cookieconter : MonoBehaviour
+{
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        gamecontroller.instance.totalScore += score;
+        gamecontroller.instance.UpdateScoreText();
+    }
+}
---
+++ Coesa/Assets/Scripts/gamecontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamecontroller : MonoBehaviour
+{
+    public int totalScore;
+    public Text scoreText;
+
+    public static gamecontroller instance; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = totalScore.ToString();
+
+    }
+
+}
